27e00a99226bcf657b0d55493973a855597bc601
ARauf Git <umiasad2017@gmail.com>
2020-03-24 07:11:48 -0400

    create a RDBMS

---
+++ sqlFile/shema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS cms;
+
+CREATE database emp_manage_sysDB;
+
+USE emp_manage_sysDB;
+
+CREATE TABLE department (
+  id INTEGER NOT NULL auto_increment,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id INTEGER NOT NULL auto_increment,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INTEGER,
+  PRIMARY KEY(id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INTEGER NOT NULL auto_increment,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INTEGER,
+  manager_id INTEGER,
+  PRIMARY KEY(id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES role(id)
+);
+
