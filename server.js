a1f95e05eb93c51b1f787960a42e7cd1c820ba26
ARauf Git <umiasad2017@gmail.com>
2020-03-29 17:35:32 -0400

    added main function to execute or queries functions

--- server.js
+++ server.js
@@ -73,20 +73,155 @@ class RDBMS {
                   "View all departments Info",
                   "View all employees Info",
                   "View all employees by department",
-                  "View all roles Data",
+                  "View all roles data",
+                  "View all managers",
+                  "View all Department Job Titles with salaries",
+                  "View all employee with salaries",
                   "Exit"
                 ]
             }
         ])
   }
 
- 
+  //Calling our main asynch function based on user selected answer
+  main();
+  async function main() {
+    let exitLoop = false;
+    while(!exitLoop) {
+        const prompt = await mainPrompt();
+  
+        switch(prompt.action) {
+            case 'Choose to Add department': {
+              console.log(
+                chalk.yellow(
+                  figlet.textSync('New Dept Data', { font:'small', horizontalLayout: 'full' })
+                )
+              );
+                const newDepartmentName = await getDepartmentInfo();
+                await addDepartment(newDepartmentName);
+                break;
+            }
+  
+            case 'Choose to Add employee': {
+              console.log(
+                chalk.blueBright(
+                  figlet.textSync('Add Employee', { font:'small', horizontalLayout: 'full' })
+                )
+              );
+                const newEmployee = await getAddEmployeeInfo();
+                console.log("add an employee");
+                console.log(newEmployee);
+                await addEmployee(newEmployee);
+                break;
+            }
+  
+            case 'Choose to Add role': {
+              console.log(
+                chalk.greenBright(
+                  figlet.textSync('Adding Role', { font:'small', horizontalLayout: 'full' })
+                )
+              );
+                const newRole = await getRoleInfo();
+                console.log("add a role");
+                await addRole(newRole);
+                break;
+            }
+  
+            case 'Choose to Remove employee record': {
+              console.log(
+                chalk.red(
+                  figlet.textSync('Removing Data', { font:'small', horizontalLayout: 'full' })
+                )
+              );
+                const employee = await getRemoveEmployeeInfo();
+                await removeEmployee(employee);
+                break;
+            }
+            
+            case 'Choose to Update employee role': {
+              console.log(
+                chalk.red(
+                  figlet.textSync('Update role', { font:'small', horizontalLayout: 'full' })
+                )
+              );
+                const employee = await getUpdateEmployeeRoleInfo();
+                await updateEmployeeRole(employee);
+                break;
+            }
+  
+            case 'View all departments Info': {
+              console.log(
+                chalk.redBright(
+                  figlet.textSync('Departments', { font:'small', horizontalLayout: 'full' })
+                )
+              );
+                await viewAllDepartments();
+                break;
+            }
+  
+            case 'View all employees Info': {
+              console.log(
+                chalk.blueBright(
+                  figlet.textSync('Employees', { font:'small', horizontalLayout: 'full' })
+                )
+              );
+                await viewAllEmployees();
+                break;
+            }
+  
+            case 'View all employees by department': {
+                await viewAllEmployeesByDepartment();
+                break;
+            }
+  
+            case 'View all roles data': {
+              console.log(
+                chalk.yellowBright(
+                  figlet.textSync('Roles', { font:'small', horizontalLayout: 'full' })
+                )
+              );
+                await viewAllRoles();
+                break;
+            }
+
+            case 'View all managers': {
+              console.log(
+                chalk.greenBright(
+                  figlet.textSync('Managers', { font:'small', horizontalLayout: 'full' })
+                )
+              );
+              await viewAllManagers();
+              break;
+          }
+
+          case 'View all Department Job Titles with salaries': {
+            await viewDepartmentBySalary();
+            break;
+        }
+
+        case 'View all employee with salaries': {
+          console.log(
+            chalk.greenBright(
+              figlet.textSync('Employees with Salaries', { font:'small', horizontalLayout: 'full' })
+            )
+          );
+          await viewAllEmployeeBySalaries();
+          break;
+      }
+  
+            case 'Exit': {
+                process.exit(0);
+            }
+        }
+    }
+  }
+
 /*
   Start of calls to the RDBMS 
 */
 
 /*Asynchronous function that will give us the list of managers each time we add an employee so we can 
-  assign manager to every new employee that we are pushing into an return array*/
+  an manager to every new employee that we are pushing into an return array*/
     async function getManagerNames() {
     let query = "SELECT * FROM employee WHERE manager_id IS NULL";
     const empTable = await conn.query(query);
@@ -166,6 +301,33 @@ class RDBMS {
     async function viewAllRoles() {
     console.log("");
     let query = "SELECT * FROM role";
+    const data = await conn.query(query);
+    console.table(data);
+    return data;
+  }
+
+  /* Asynchronous fuunction that will display all the managers names */
+  async function viewAllManagers() {
+    console.log("");
+    let query = "SELECT * FROM employee where manager_id IS NULL";
+    const data = await conn.query(query);
+    console.table(data);
+    return data;
+  }
+
+  /* Asynchronous fuunction that will display all the employee and their salaries */
+  async function viewAllEmployeeBySalaries() {
+    console.log("");
+    let query = "select first_name, last_name, salary from employee, role where employee.role_id = role.id";
+    const data = await conn.query(query);
+    console.table(data);
+    return data;
+  }
+
+  /* Asynchronous fuunction that will display each department with positions with respect to there salary */
+  async function viewDepartmentBySalary() {
+    console.log("");
+    let query = "select name as department, title,salary from department,role where department.id=department_id";
     const data = await conn.query(query);
     console.table(data);
     return data;
@@ -269,6 +431,134 @@ class RDBMS {
 /* 
 Done here integrating with the mysql database
 */
+
+async function getAddEmployeeInfo() {
+  const managers = await getManagerNames();
+  const roles = await getRoles();
+  return inquirer
+      .prompt([
+          {
+              type: "input",
+              name: "first_name",
+              message: "What is the employee's first name?"
+          },
+          {
+              type: "input",
+              name: "last_name",
+              message: "What is the employee's last name?"
+          },
+          {
+              type: "list",
+              message: "What is the employee's role?",
+              name: "role",
+              choices: [
+                  // populate from RDBMS
+                  ...roles
+              ]
+          },
+          {
+              type: "list",
+              message: "Who is the employee's manager?",
+              name: "manager",
+              choices: [
+                  // populate from RDBMS
+                  ...managers
+              ]
+          }
+      ])
+}
+
+async function getRemoveEmployeeInfo() {
+  const employees = await getEmployeeNames();
+  return inquirer
+  .prompt([
+      {
+          type: "list",
+          message: "Which employee do you want to remove?",
+          name: "employeeName",
+          choices: [
+              // populate from RDBMS
+              ...employees
+          ]
+      }
+  ])
+}
+
+async function getDepartmentInfo() {
+  return inquirer
+  .prompt([
+      {
+          type: "input",
+          message: "What is the name of the new department?",
+          name: "departmentName"
+      }
+  ])
+}
+
+async function getRoleInfo() {
+  const departments = await getDepartmentNames();
+  return inquirer
+  .prompt([
+      {
+          type: "input",
+          message: "What is the title of the new role?",
+          name: "roleName"
+      },
+      {
+          type: "input",
+          message: "What is the salary of the new role?",
+          name: "salary"
+      },
+      {
+          type: "list",
+          message: "Which department uses this role?",
+          name: "departmentName",
+          choices: [
+              // populate from RDBMS
+              ...departments
+          ]
+      }
+  ])
+}
+
+async function getUpdateEmployeeRoleInfo() {
+  const employees = await getEmployeeNames();
+  const roles = await getRoles();
+  return inquirer
+      .prompt([
+          {
+              type: "list",
+              message: "Which employee do you want to update?",
+              name: "employeeName",
+              choices: [
+                  // populate from RDBMS
+                  ...employees
+              ]
+          },
+          {
+              type: "list",
+              message: "What is the employee's new role?",
+              name: "role",
+              choices: [
+                  // populate from RDBMS
+                  ...roles
+              ]
+          }
+      ])
+
+}
+
+
+// Close your database connection when Node exits
+process.on("exit", async function(code) {
+  await db.close();
+  return console.log(`About to exit with code ${code}`);
+});
+
+
+
+
+
 
 
 
